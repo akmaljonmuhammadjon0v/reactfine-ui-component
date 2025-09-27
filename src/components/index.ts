a898cc11f1d4c162dcd9f861e469855222a0de87
akmaljon <akmal@gmail.com>
2025-09-27 14:59:48 +0500

    bugs fixed

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
 import '../index.css';
 export * from './input'; // input/index.ts
-export { default as Button } from './button'; // button/index.ts
+export * from './button'; // button/index.ts
 export * from './badge'; // badge/index.ts
 export * from './modal'; // modal/index.ts
 export * from './card'; // card/index.ts
